be6e9f2bbd21abf8b02a14d3537057235444d74e
Simon Duck <its.duck@gmail.com>
2025-05-08 12:55:37 +0100

    pop up menu stage 1

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Add your JavaScript here.
 });
+
+const colorButton = document.getElementById("color-button")
+
+colorButton.addEventListener("click", randomColor);
+
+
+function randomColor() {
+
+  // const postCards = document.getElementsByClassName("postCard");
+  const postCards = document.getElementsByTagName("li")
+ for (i=0; i<postCards.length; i++) {
+  postCards[i].style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
+  console.log(postCards)
+ }
+
+}
+
+const menuButton = document.getElementById("menu-button");
+const navBar = document.getElementsByClassName("navBar");
+console.log(navBar)
+
+menuButton.addEventListener("click", makeMenuVisible);
+
+function makeMenuVisible() {
+
+  navBar[0].setAttribute("id", "ourNav")}
