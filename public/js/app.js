3a32f415dff35c68f4568ea22fc8b4c481039b80
Simon Duck <its.duck@gmail.com>
2025-05-08 14:39:58 +0100

    click away closes menu

--- public/js/app.js
+++ public/js/app.js
@@ -20,10 +20,33 @@ function randomColor() {
 
 const menuButton = document.getElementById("menu-button");
 const navBar = document.getElementsByClassName("navBar");
+
 console.log(navBar)
 
 menuButton.addEventListener("click", makeMenuVisible);
 
 function makeMenuVisible() {
 
-  navBar[0].setAttribute("id", "ourNav")}
+const menuShown = navBar[0].getAttribute("id")
+ console.log(menuShown)
+
+   if (menuShown ) {
+    navBar[0].removeAttribute("id"); 
+  }
+  else
+  {
+  navBar[0].setAttribute("id", "ourNav");
+}
+}
+
+
+document.addEventListener("click", clickAwayHideMenu);
+
+function clickAwayHideMenu (e) {
+  const navBar = document.getElementsByClassName("navBar");
+  const menuButton = document.getElementById("menu-button");
+  if (!navBar[0].contains(e.target) && !menuButton.contains(e.target))
+    {
+    navBar[0].removeAttribute("id"); 
+  }
+}
